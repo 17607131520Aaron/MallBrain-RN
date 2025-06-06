f441df8f535925f5ae3e758759fe340695cb3843
huangyafeng <p-huangyafeng@xiaomi.com>
2025-06-06 20:04:00 +0800

    feat: init新增按钮组件

---
+++ src/components/Button/index.style.ts
@@ -0,0 +1,80 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '~/constants/colors';
+
+const styles = StyleSheet.create({
+  button: {
+    height: 36,
+    flexDirection: 'row',
+    backgroundColor: COLORS.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 18,
+    paddingHorizontal: 10,
+  },
+  text: {
+    fontSize: 16,
+  },
+  disabled: {
+    backgroundColor: '#B2B4BF',
+  },
+  cancel: {
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.border,
+    borderWidth: 1,
+  },
+  default: {
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.border,
+    borderWidth: 1,
+  },
+  primaryLine: {
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.primary,
+    borderWidth: 1,
+  },
+  errorLine: {
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.error,
+    borderWidth: 1,
+  },
+  primary: {
+    backgroundColor: COLORS.primary,
+  },
+  warning: {
+    backgroundColor: COLORS.warn,
+  },
+  error: {
+    backgroundColor: COLORS.error,
+  },
+  defaultText: {
+    color: COLORS.title,
+  },
+  cancelText: {
+    color: COLORS.content,
+  },
+  primaryLineText: {
+    color: COLORS.primary,
+  },
+  primaryText: {
+    color: COLORS.white,
+  },
+  errorLineText: {
+    color: COLORS.error,
+  },
+  warningText: {
+    color: COLORS.white,
+  },
+  errorText: {
+    color: COLORS.white,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  loading: {
+    width: 16,
+    height: 16,
+    marginRight: 4,
+  },
+});
+
+export default styles;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Button from '~/components/Button';
+import { MessageType, showMessage } from '~/utils/message';
 import styles from './index.style';
 const Home: React.FC = () => {
+  const handleConfirm = () => {
+    showMessage({
+      type: MessageType.ERROR,
+      message: '确认录入',
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>首页asdasd</Text>
+
+      <Button type='primary' children='确认录入' onPress={handleConfirm} />
     </View>
   );
 };
