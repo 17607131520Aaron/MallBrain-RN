9cb879dd897b830f6e860c55023596d7eca0c876
huangyafeng <yafenghuang777@gmail.com>
2025-06-07 18:35:28 +0800

    移除 @react-native-community/async-storage 依赖，更新相关文件以使用 @react-native-async-storage/async-storage，优化服务页面，新增测试按钮功能，提升代码可读性和可维护性。

--- src/pages/Service/index.tsx
+++ src/pages/Service/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import { Button } from '~/components/Button';
+import { EMessageType, showMessage } from '~/utils/message';
+
 import styles from './index.style';
 
 const Service: React.FC = () => (
   <View style={styles.container}>
     <Text>服务</Text>
+    <Button
+      type='primary'
+      onPress={() => {
+        showMessage({
+          type: EMessageType.SUCCESS,
+          message: '测试',
+        });
+      }}
+    >
+      <Text>测试</Text>
+    </Button>
   </View>
 );
 
