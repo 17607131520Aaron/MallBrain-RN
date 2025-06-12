8824570fc21c8d9288e5ea420ced82a86955e0d2
huangyafeng <p-huangyafeng@xiaomi.com>
2025-06-12 13:49:09 +0800

    新增 Flipper 调试工具支持，更新相关依赖和配置，优化 ReactNativeFlipper 类以提升代码可维护性和稳定性。

--- android/app/src/debug/java/com/mynewproject/ReactNativeFlipper.kt
+++ android/app/src/debug/java/com/mynewproject/ReactNativeFlipper.kt
@@ -1,29 +1,54 @@
 package com.mynewproject
 
 import android.content.Context
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.modules.network.NetworkingModule
 
-class ReactNativeFlipper {
-    companion object {
-        fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
-            try {
-                // 获取自定义的Metro端口
-                val metroPort = context.resources.getString(R.string.metro_port)
-                val defaultPort = "8088"
-                val port = if (metroPort.isNotEmpty()) metroPort else defaultPort
+/**
+ * Class responsible for loading Flipper inside your React Native application.
+ * This is the implementation for React Native 0.79.2
+ */
+object ReactNativeFlipper {
+    fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
+        if (!FlipperUtils.shouldEnableFlipper(context)) {
+            return
+        }
 
-                // 设置自定义端口
-                reactInstanceManager.addReactInstanceEventListener(object : ReactInstanceManager.ReactInstanceEventListener {
-                    override fun onReactContextInitialized(reactContext: ReactContext) {
-                        reactContext.catalystInstance.setGlobalVariable("Metro_Port", port)
-                        reactInstanceManager.removeReactInstanceEventListener(this)
-                    }
-                })
-            } catch (e: Exception) {
-                // 捕获初始化错误，防止应用崩溃
-                e.printStackTrace()
-            }
+        val client: FlipperClient = AndroidFlipperClient.getInstance(context)
+
+        client.addPlugin(InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()))
+        client.addPlugin(DatabasesFlipperPlugin(context))
+        client.addPlugin(SharedPreferencesFlipperPlugin(context))
+        client.addPlugin(CrashReporterPlugin.getInstance())
+
+        val networkFlipperPlugin = NetworkFlipperPlugin()
+        NetworkingModule.setCustomClientBuilder { builder ->
+            builder.addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
+        }
+        client.addPlugin(networkFlipperPlugin)
+
+        // For Fresco plugin
+        if (reactInstanceManager.currentReactContext == null) {
+            reactInstanceManager.addReactInstanceEventListener(object : ReactInstanceManager.ReactInstanceEventListener {
+                override fun onReactContextInitialized(reactContext: ReactContext) {
+                    reactInstanceManager.removeReactInstanceEventListener(this)
+                    client.start()
+                }
+            })
+        } else {
+            client.start()
         }
     }
 }
